faf2cd4a073e5ecec6a2b015cfde12962a991bc4
Vetula_Mortem <renesebestian@gmail.com>
2017-02-02 14:49:47 +0100

    Witz anrede

--- src/rsebestian/Hallo.java
+++ src/rsebestian/Hallo.java
@@ -15,7 +15,7 @@ public class Hallo {
 	}
 	
 	public void helloName(){
-		System.out.println("Hallo " + getN());
+		System.out.println("Hallo " + getN() + " ich erzähle dir mal nen Witz");
 	}
 	public void witzName(){
 		System.out.println("Wer geht über die Straße? \nNatürlich " + getN());
